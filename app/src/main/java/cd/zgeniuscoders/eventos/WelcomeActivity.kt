dac88e9002a1f67bc19852b3df128bed82976256
zgeniuscoders <zgeniuscoders.com>
2024-04-07 13:51:15 +0100

    end auth ui features

--- app/src/main/java/cd/zgeniuscoders/eventos/WelcomeActivity.kt
+++ app/src/main/java/cd/zgeniuscoders/eventos/WelcomeActivity.kt
@@ -1,20 +1,31 @@
 package cd.zgeniuscoders.eventos
 
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import cd.zgeniuscoders.eventos.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_welcome)
+
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
 }
