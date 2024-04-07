67204841cfd18b045e927ffd1b95934a711b27ef
zgeniuscoders <zgeniuscoders.com>
2024-04-07 22:03:36 +0100

    start all event activity

--- app/src/main/java/cd/zgeniuscoders/eventos/AllEventActivity.kt
+++ app/src/main/java/cd/zgeniuscoders/eventos/AllEventActivity.kt
@@ -5,26 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
-import cd.zgeniuscoders.eventos.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class AllEventActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        setContentView(R.layout.activity_all_event)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val navController = findNavController(R.id.main_fragment)
-        NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
     }
 }
--- app/src/main/java/cd/zgeniuscoders/eventos/WelcomeActivity.kt
+++ app/src/main/java/cd/zgeniuscoders/eventos/WelcomeActivity.kt
@@ -17,15 +17,8 @@ class WelcomeActivity : AppCompatActivity() {
         enableEdgeToEdge()
 
         binding = ActivityWelcomeBinding.inflate(layoutInflater)
-        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-
-
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
 
     }
 }
