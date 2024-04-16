5d96af8e6f12c22a0e19aa729416a0a647b67a4f
zgeniuscoders <zgeniuscoders.com>
2024-04-16 16:46:07 +0100

    ui add event

---
+++ app/src/main/java/cd/zgeniuscoders/eventos/EventActivity.kt
@@ -0,0 +1,20 @@
+package cd.zgeniuscoders.eventos
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class EventActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_event)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
