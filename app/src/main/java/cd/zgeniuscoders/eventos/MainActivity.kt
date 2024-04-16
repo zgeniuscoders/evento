b02c825c6047c91401465654a2fb52a90501f6d3
zgeniuscoders <zgeniuscoders.com>
2024-04-16 16:55:43 +0100

    fixed navigationBar height

--- app/src/main/java/cd/zgeniuscoders/eventos/MainActivity.kt
+++ app/src/main/java/cd/zgeniuscoders/eventos/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
